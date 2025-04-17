09f363b824be034eb49c64085241385303fb58a0
Tornrosen <elec11anna@hotmail.com>
2025-04-17 11:44:26 +0200

    Skapa utvecklingsmiljö o grund html, css, meny

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let openBtn = document.getElementById("openMenu");
+let closeBtn = document.getElementById("closeMenu");
+
+openBtn.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", toggleMenu);
+
+/**
+ * visar eller döljer menyn vid ett klick
+ * 
+ * function toggleMenu
+ * @param {} 
+ * @returns void
+ */
+
+function toggleMenu() {
+    let navMenuEl = document.getElementById("navMenu");
+
+    let style = window.getComputedStyle(navMenuEl);
+
+    if (style.display === "none") {
+        navMenuEl.style.display = "block";
+    } else {
+        navMenuEl.style.display = "none";
+    }
+}
