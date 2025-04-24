acc5beab157968f78fc6ecf048d628710b0959e1
Tornrosen <elec11anna@hotmail.com>
2025-04-24 15:34:53 +0200

    fixa funktioner för att kolla input o lägga till

--- dist/add.bde1fdf4.js
+++ dist/add.bde1fdf4.js
@@ -673,22 +673,28 @@ let url = "http://127.0.0.1:3000/api/jobs";
 let submitBtnEl = document.querySelector("#submitBtn");
 let descriptionEl = document.querySelector("#description");
 let errorSpaceEl = document.querySelector("#errorSpace");
-let title = document.querySelector("#title").value;
-let workplace = document.querySelector("#workplace").value;
-let employer = document.querySelector("#employer").value;
-let startdate = document.querySelector("#startdate").value;
-let enddate = document.querySelector("#enddate").value;
-let description = document.querySelector("#description").value;
 //skapa händelsehanterare för att lägga till jobb och kolla input
 submitBtnEl.addEventListener("click", addJob);
-//descriptionEl.addEventListener("keyup", checkInput);
+descriptionEl.addEventListener("keyup", checkInput);
 //funktion för att kontrollera längd på input
-/*function checkInput() {
-    if(title.length<1)
-        {errorSpaceEl.innerHTML= "Fyll i alla textfält!";}
-    else {errorSpaceEl.innerHTML='';}
-}*/ //funktion för att lägga till jobb
+function checkInput() {
+    let title = document.querySelector("#title").value;
+    let workplace = document.querySelector("#workplace").value;
+    let employer = document.querySelector("#employer").value;
+    let startdate = document.querySelector("#startdate").value;
+    let enddate = document.querySelector("#enddate").value;
+    let description = document.querySelector("#description").value;
+    if (title.length < 1 || workplace.length < 1 || employer.length < 1 || startdate.length < 1 || enddate.length < 1 || description.length < 1) errorSpaceEl.innerHTML = "Fyll i alla textf\xe4lt!";
+    else errorSpaceEl.innerHTML = '';
+}
+//funktion för att lägga till jobb
 async function addJob() {
+    let title = document.querySelector("#title").value;
+    let workplace = document.querySelector("#workplace").value;
+    let employer = document.querySelector("#employer").value;
+    let startdate = document.querySelector("#startdate").value;
+    let enddate = document.querySelector("#enddate").value;
+    let description = document.querySelector("#description").value;
     let job = {
         title: title,
         workplace: workplace,
